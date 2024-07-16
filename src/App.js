93c54f49b49eade4a627e7029e518190ff5d2b41
Abdul Ahad <97602347+abdulahad0311@users.noreply.github.com>
2024-07-17 01:42:06 +0500

    kaaam

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './includes/Header'
+import { Casestudy } from './includes/Casestudy';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Casestudy />
 		</div>
 	);
 }
---
+++ src/includes/Casestudy.js
@@ -0,0 +1,53 @@
+export const Casestudy = () => {
+    return(
+        <>
+        <div id="page4">
+            <div className="section">
+                <div className="sec-left">
+                <h2>Accern. Rhea</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+                    inventore enim, quos, qui est id, architecto libero in esse vel officiis
+                    repellat alias maxime minima.
+                </p>
+                </div>
+                <div className="sec-right">
+                <img
+                    src="https://assets-global.website-files.com/64a5e6675a4159253d0ee3e0/65a93ac90b586168e5917ac8_Rhea%20-%20cover_horizontal_low.webp"
+                    alt=""
+                />
+                <video muted="" src="./accern-rhea-cover-big.mp4" />
+                </div>
+            </div>
+            <div className="section">
+                <div className="sec-left">
+                <h2>AfroTech</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+                    inventore enim, quos, qui est id, architecto libero in esse vel officiis
+                    repellat alias maxime minima.
+                </p>
+                </div>
+                <div className="sec-right">
+                <img
+                    src="https://assets-global.website-files.com/64a5e6675a4159253d0ee3e0/65cb66041ce86ac3784aaba1_Afrotech%20-%20cover_horizontal.webp"
+                    alt=""
+                />
+                <video
+                    muted=""
+                    src="https://lazarev.kiev.ua/cases/la24/afrotech-cover-big.mp4"
+                />
+                </div>
+            </div>
+            <div>
+                <div className="section">
+                <div style={{ backgroundColor: "black" }} className="col-lg-3" />
+                <div style={{ backgroundColor: "black" }} className="col-lg-3" />
+                <div style={{ backgroundColor: "black" }} className="col-lg-3" />
+                </div>
+            </div>
+        </div>
+
+        </>
+    )
+}
