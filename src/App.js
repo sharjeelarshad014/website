129c57d0dd4630f339b65fc59ab7a2e45f486b91
faheem456 <46492824+sharjeelarshad014@users.noreply.github.com>
2024-07-17 00:21:35 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './includes/Header'
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
+}
+
+export default App;
